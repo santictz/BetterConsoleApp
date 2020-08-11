4fb392a05ee4f1ce3f260981f7b090b685b56513
Santiago Cortez <cortezsantiago@outlook.com>
2020-08-10 21:11:27 -0300

    [Init] Add Project files

---
+++ ConsoleUI/GreetingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleUI
+{
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> log;
+        private readonly IConfiguration config;
+
+        /// <summary>
+        /// Initialize service with DI to enable logging and configuration
+        /// </summary>
+        /// <param name="log">ILogger instance</param>
+        /// <param name="config">IConfiguration instance</param>
+        public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+        {
+            this.log = log;
+            this.config = config;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < config.GetValue<int>("LoopTimes"); i++)
+            {
+                //Do not use string interpolation when logging
+                log.LogInformation("Run number {runNumber}", i);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BetterConsoleApp
 .NET Core Console Application with Depency Injection, HostBuilder, Serilog and App Configuration settings
+
+- Based on [Tim Corey video](https://www.youtube.com/watch?v=GAOCe-2nXqc)
